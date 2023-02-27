a76326ba9b27d13569935a8554db7b702faf8517
Trace Ahmuty <ahmutysj3@gmail.com>
2023-02-27 16:39:05 -0500

    updated cidr_block argument for subnets

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "spokes" {
   for_each = var.subnet_params
   vpc_id = aws_vpc.main[each.value.vpc].id
-  cidr_block = cidrsubnet(aws_vpc.main[each.value.vpc].cidr_block, each.value.cidr_mask - local.app_vpc_mask,lookup(local.combined_net_num,each.key))
+  cidr_block = cidrsubnet(aws_vpc.main[each.value.vpc].cidr_block, each.value.cidr_mask - lookup(local.combined_vpc_mask,each.value.vpc),lookup(local.combined_net_num,each.key))
   map_public_ip_on_launch = each.value.public
 }
 
@@ -20,19 +20,15 @@ locals {
   db_subnets = { for k, v in var.subnet_params: k => v if v.vpc == "db"}
 }
 
-# creates a map of subnet name to net bit number for cidrsubnet function
+# returns a string with the cidr net mask of each vpc
 locals {
-  app_net_num = {for k, v in keys(local.app_subnets) : v => k}
-  dmz_net_num = {for k, v in keys(local.dmz_subnets) : v => k}
-  db_net_num = {for k, v in keys(local.db_subnets) : v => k}
+  app_vpc_mask = {"app" = tonumber(element(split("/", aws_vpc.main["app"].cidr_block),1))}
+  dmz_vpc_mask = {"dmz" = tonumber(element(split("/", aws_vpc.main["dmz"].cidr_block),1))}
+  db_vpc_mask = {"db" = tonumber(element(split("/", aws_vpc.main["db"].cidr_block),1))}
+  combined_vpc_mask = merge({"app" = tonumber(element(split("/", aws_vpc.main["app"].cidr_block),1))},{"dmz" = tonumber(element(split("/", aws_vpc.main["dmz"].cidr_block),1))},{"db" = tonumber(element(split("/", aws_vpc.main["db"].cidr_block),1))})
 }
 
-locals {
-  app_vpc_mask = tonumber(element(split("/", aws_vpc.main["app"].cidr_block),1))
-  dmz_vpc_mask = tonumber(element(split("/", aws_vpc.main["dmz"].cidr_block),1))
-  db_vpc_mask = tonumber(element(split("/", aws_vpc.main["db"].cidr_block),1))
-}
-
+# creates a map of subnets to their network number (indexes by vpc)
 locals {
   combined_net_num = merge({for k, v in keys(local.app_subnets) : v => k},{for k, v in keys(local.dmz_subnets) : v => k},{for k, v in keys(local.db_subnets) : v => k})
 }
