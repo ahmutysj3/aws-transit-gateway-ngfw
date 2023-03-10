2003fe3a61656824333d45e785a4488c60238731
trace <ahmutysj3@gmail.com>
2023-03-09 23:51:06 -0500

    added route table associations back

--- main.tf
+++ main.tf
@@ -170,3 +170,27 @@ resource "aws_networkmanager_transit_gateway_registration" "trace" {
   global_network_id   = aws_networkmanager_global_network.trace.id
   transit_gateway_arn = aws_ec2_transit_gateway.trace.arn
 }
+
+##################################################################################
+//////////////////////// Subnet Route Tables /////////////////////////////////////
+##################################################################################
+
+resource "aws_route_table" "spokes" {
+  for_each = { for k, v in var.vpc_params : k => v if v.type == "spoke" }
+  vpc_id   = aws_vpc.main[each.key].id
+
+  route {
+    cidr_block         = "0.0.0.0/0"
+    transit_gateway_id = aws_ec2_transit_gateway.trace.id
+  }
+
+  tags = {
+    Name = "${var.net_name}_${each.key}_subnet_rt"
+  }
+}
+
+resource "aws_route_table_association" "spokes" {
+  for_each       = var.subnet_params
+  subnet_id      = aws_subnet.spokes[each.key].id
+  route_table_id = aws_route_table.spokes[each.value.vpc].id
+}
