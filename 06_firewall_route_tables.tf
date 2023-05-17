5f8b95ef51d4959861aa5a2551260edeefca4388
Trace Ahmuty <ahmutysj3@gmail.com>
2023-05-17 17:14:37 -0400

    updated routes

--- 06_firewall_route_tables.tf
+++ 06_firewall_route_tables.tf
@@ -1,6 +1,6 @@
 
 # firewall Route Tables
-resource "aws_route_table" "fw_internal_pri" {
+resource "aws_route_table" "fw_internal" {
   vpc_id = aws_vpc.firewall_vpc.id
 
   tags = {
@@ -8,7 +8,7 @@ resource "aws_route_table" "fw_internal_pri" {
   }
 }
 
-resource "aws_route_table" "fw_external_pri" {
+resource "aws_route_table" "fw_external" {
   vpc_id = aws_vpc.firewall_vpc.id
 
   tags = {
@@ -16,60 +16,63 @@ resource "aws_route_table" "fw_external_pri" {
   }
 }
 
-resource "aws_route_table" "fw_internal_sec" {
+resource "aws_route_table" "fw_tgw" {
   vpc_id = aws_vpc.firewall_vpc.id
 
   tags = {
-    Name = "fw_internal_route_table_sec"
+    Name = "fw_tgw_route_table"
   }
 }
 
-resource "aws_route_table" "fw_external_sec" {
-  vpc_id = aws_vpc.firewall_vpc.id
+# Firewall TGW Route Table Associations
+resource "aws_route_table_association" "fw_tgw_pri" {
+  subnet_id      = aws_subnet.tgw_pri.id
+  route_table_id = aws_route_table.fw_tgw.id
+}
 
-  tags = {
-    Name = "fw_external_route_table_sec"
-  }
+resource "aws_route_table_association" "fw_tgw_sec" {
+  subnet_id      = aws_subnet.tgw_sec.id
+  route_table_id = aws_route_table.fw_tgw.id
 }
 
 # Firewall External Route Table Associations
 resource "aws_route_table_association" "fw_mgmt_pri" {
   subnet_id      = aws_subnet.fw_mgmt_pri.id
-  route_table_id = aws_route_table.fw_external_pri.id
+  route_table_id = aws_route_table.fw_external.id
 }
 
 resource "aws_route_table_association" "fw_mgmt_sec" {
   subnet_id      = aws_subnet.fw_mgmt_sec.id
-  route_table_id = aws_route_table.fw_external_sec.id
+  route_table_id = aws_route_table.fw_external.id
 }
 
 resource "aws_route_table_association" "fw_outside_pri" {
   subnet_id      = aws_subnet.fw_outside_pri.id
-  route_table_id = aws_route_table.fw_external_pri.id
+  route_table_id = aws_route_table.fw_external.id
 }
 
 resource "aws_route_table_association" "fw_outside_sec" {
   subnet_id      = aws_subnet.fw_outside_sec.id
-  route_table_id = aws_route_table.fw_external_sec.id
+  route_table_id = aws_route_table.fw_external.id
 }
 
 # Firewall Internal Route Table Associations
 resource "aws_route_table_association" "fw_inside_pri" {
   subnet_id      = aws_subnet.fw_inside_pri.id
-  route_table_id = aws_route_table.fw_internal_pri.id
+  route_table_id = aws_route_table.fw_internal.id
 }
 
 resource "aws_route_table_association" "fw_inside_sec" {
   subnet_id      = aws_subnet.fw_inside_sec.id
-  route_table_id = aws_route_table.fw_internal_sec.id
+  route_table_id = aws_route_table.fw_internal.id
 }
 
 resource "aws_route_table_association" "fw_heartbeat_pri" {
   subnet_id      = aws_subnet.fw_heartbeat_pri.id
-  route_table_id = aws_route_table.fw_internal_pri.id
+  route_table_id = aws_route_table.fw_internal.id
 }
 
 resource "aws_route_table_association" "fw_heartbeat_sec" {
   subnet_id      = aws_subnet.fw_heartbeat_sec.id
-  route_table_id = aws_route_table.fw_internal_sec.id
+  route_table_id = aws_route_table.fw_internal.id
 }
