ace0e62e2ed2a6caf17ef1d946aa50c3abfb0bf4
Trace Ahmuty <ahmutysj3@gmail.com>
2023-05-17 15:26:42 -0400

    added tgw routes to tgw rt tables

--- main.tf
+++ main.tf
@@ -295,10 +295,40 @@ resource "aws_ec2_transit_gateway" "main" {
 # Transit Gateway Route Tables
 resource "aws_ec2_transit_gateway_route_table" "spoke" {
   transit_gateway_id = aws_ec2_transit_gateway.main.id
+  tags = {
+    Name = "tgw_spoke_route_table"
+  }
 }
 
 resource "aws_ec2_transit_gateway_route_table" "hub" {
   transit_gateway_id = aws_ec2_transit_gateway.main.id
+  tags = {
+    Name = "tgw_hub_route_table"
+  }
+}
+
+resource "aws_ec2_transit_gateway_route" "spoke_a" {
+  destination_cidr_block = "0.0.0.0/0"
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.spoke.id
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.spoke_a.id
+}
+
+resource "aws_ec2_transit_gateway_route" "spoke_b" {
+  destination_cidr_block = "0.0.0.0/0"
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.spoke.id
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.spoke_b.id
+}
+
+resource "aws_ec2_transit_gateway_route" "hub_to_spoke_a" {
+  destination_cidr_block = "10.1.1.0/24"
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.hub.id
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.spoke_a.id
+}
+
+resource "aws_ec2_transit_gateway_route" "hub_to_spoke_b" {
+  destination_cidr_block = "10.2.1.0/24"
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.hub.id
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.spoke_b.id
 }
 
 # Transit Gateway Route Table Associations
