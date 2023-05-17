fb046d88f9a5a99d62ae518d22a617f618fa71d2
Trace Ahmuty <ahmutysj3@gmail.com>
2023-05-17 16:46:56 -0400

    fixed typo for tgw name ref

--- 04_spoke_routes.tf
+++ 04_spoke_routes.tf
@@ -1,11 +1,11 @@
 resource "aws_route" "spoke_a" {
   route_table_id         = aws_route_table.spoke_a.id
   destination_cidr_block = "0.0.0.0/0"
-  transit_gateway_id     = aws_ec2_transit_gateway.firewall.id
+  transit_gateway_id     = aws_ec2_transit_gateway.main.id
 }
 
 resource "aws_route" "spoke_b" {
   route_table_id         = aws_route_table.spoke_b.id
   destination_cidr_block = "0.0.0.0/0"
-  transit_gateway_id     = aws_ec2_transit_gateway.firewall.id
+  transit_gateway_id     = aws_ec2_transit_gateway.main.id
 }
--- 07_firewall_routes.tf
+++ 07_firewall_routes.tf
@@ -8,11 +8,11 @@ resource "aws_route" "fw_external_inet" {
 resource "aws_route" "fw_external_spoke_a" {
   route_table_id         = aws_route_table.fw_external_pri.id
   destination_cidr_block = "10.1.1.0/24"
-  transit_gateway_id     = aws_ec2_transit_gateway.firewall.id
+  transit_gateway_id     = aws_ec2_transit_gateway.main.id
 }
 
 resource "aws_route" "fw_external_spoke_b" {
   route_table_id         = aws_route_table.fw_external_pri.id
   destination_cidr_block = "10.2.1.0/24"
-  transit_gateway_id     = aws_ec2_transit_gateway.firewall.id
+  transit_gateway_id     = aws_ec2_transit_gateway.main.id
 }
