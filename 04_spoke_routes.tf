675ba6bfc762cf77dd3d92c38406c63c9250af77
Trace Ahmuty <ahmutysj3@gmail.com>
2023-05-17 16:49:44 -0400

    fix resource name

--- 04_spoke_routes.tf
+++ 04_spoke_routes.tf
@@ -1,11 +1,11 @@
 resource "aws_route" "spoke_a" {
-  route_table_id         = aws_route_table.spoke_a.id
+  route_table_id         = aws_route_table.spoke_a_subnet.id
   destination_cidr_block = "0.0.0.0/0"
   transit_gateway_id     = aws_ec2_transit_gateway.main.id
 }
 
 resource "aws_route" "spoke_b" {
-  route_table_id         = aws_route_table.spoke_b.id
+  route_table_id         = aws_route_table.spoke_b_subnet.id
   destination_cidr_block = "0.0.0.0/0"
   transit_gateway_id     = aws_ec2_transit_gateway.main.id
 }
