08c0036fe5bd613635d0a6f222477fbe61682a4d
Trace Ahmuty <ahmutysj3@gmail.com>
2023-05-22 17:14:34 -0400

    added count index to flow logs

--- main.tf
+++ main.tf
@@ -485,7 +485,7 @@ resource "aws_eip" "fw_outside" {
 }
 
 resource "aws_s3_bucket" "flow_logs" {
-  count         = 1
+  count = 1
   bucket        = "${var.network_prefix}-vpc-flow-logs"
   force_destroy = true
 
@@ -497,7 +497,7 @@ resource "aws_s3_bucket" "flow_logs" {
 
 resource "aws_flow_log" "spoke_vpc_a" {
   count                = 1
-  log_destination      = aws_s3_bucket.flow_logs.arn
+  log_destination      = aws_s3_bucket.flow_logs[0].arn
   log_destination_type = "s3"
   traffic_type         = "ALL"
   vpc_id               = aws_vpc.spoke_vpc_a.id
@@ -505,7 +505,7 @@ resource "aws_flow_log" "spoke_vpc_a" {
 
 resource "aws_flow_log" "spoke_vpc_b" {
   count                = 1
-  log_destination      = aws_s3_bucket.flow_logs.arn
+  log_destination      = aws_s3_bucket.flow_logs[0].arn
   log_destination_type = "s3"
   traffic_type         = "ALL"
   vpc_id               = aws_vpc.spoke_vpc_b.id
@@ -513,7 +513,7 @@ resource "aws_flow_log" "spoke_vpc_b" {
 
 resource "aws_flow_log" "firewall_vpc" {
   count                = 1
-  log_destination      = aws_s3_bucket.flow_logs.arn
+  log_destination      = aws_s3_bucket.flow_logs[0].arn
   log_destination_type = "s3"
   traffic_type         = "ALL"
   vpc_id               = aws_vpc.firewall_vpc.id
