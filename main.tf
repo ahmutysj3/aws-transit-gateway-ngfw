7cf9eea97368880cac57e3c1c225a78db66a2bda
Trace Ahmuty <ahmutysj3@gmail.com>
2023-05-27 13:24:41 -0400

    terraorm fmt

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "tgw_network" {
   source                   = "./module"
-  region_aws               = "us-east-1"
   network_prefix           = "trace"
   supernet_cidr            = "10.200.0.0/16"
+  region_aws               = var.region_aws
   availability_zone_list   = data.aws_availability_zones.available.names
   fortigate_ami            = data.aws_ami.fortigate
   spoke_vpc_params         = var.spoke_vpc_params
