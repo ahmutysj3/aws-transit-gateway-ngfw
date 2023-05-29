ea5f1b583e4b5360b22652f8b87e2a03334d43fb
Trace Ahmuty <ahmutysj3@gmail.com>
2023-05-29 11:37:57 -0400

    tf fmt

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "network" {
   network_prefix           = "trace"
   supernet_cidr            = "10.200.0.0/16"
   region_aws               = var.region_aws
-  iam_policy_assume_role = data.aws_iam_policy_document.assume_role
+  iam_policy_assume_role   = data.aws_iam_policy_document.assume_role
   iam_policy_flow_logs     = data.aws_iam_policy_document.flow_logs
   availability_zone_list   = data.aws_availability_zones.available.names
   fortigate_ami            = data.aws_ami.fortigate
